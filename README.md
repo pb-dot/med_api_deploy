7e124ba5d312c7d804a4a4bf44b2bbfd47a2ca34
pb-dot <pb.20u10792@btech.nitdgp.ac.in>
2023-07-31 03:26:45 +0530

    changed to streamlit from flask

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# med_api_deploy
-## This is solely for production purporses
-## To run locally
-pip install -r requirements.txt --user <br>
-python app.py
-## The main code is found <a href="https://github.com/pb-dot/med_api_code"> here </a>
+First Go to the helpers folder to run the gen1.py and gen2.py to generate the model.pkl files
+ie SVM model and KNN model with tfidf text representation in gen1.py
+and LR model which uses transformer as text representation in gen2.py
+
+The util.py has hepler function needed by app.py for prediction and text preprocessing
+
+While deploying remove the helpers folder as we already have .pkl files
+
+############ Run the whole application using
+pip install -r requirements.txt --user
+streamlit run app.py
--- app.py
+++ app.py
@@ -1,68 +1,75 @@
 # API server using Flask
-from flask import Flask, request, jsonify, render_template
+import streamlit as st
 from sentence_transformers import SentenceTransformer
 from util import * # contains helper functions
-
-
 #loading the models 
 import joblib
-model1 = joblib.load('svm_model.pkl')
-model2 = joblib.load('knn_model.pkl')
-model3 = joblib.load('LR_model.pkl')
+
+st.header(" Med_API By PB")
+query = False
+options = st.radio(
+    'Choose model',
+    ('LR + transformer','SVM + tfidf','KNN + tfidf'))
+
+if 'SVM + tfidf' in options:
+    model1 = joblib.load('svm_model.pkl')
+if 'KNN + tfidf' in options:
+    model2 = joblib.load('knn_model.pkl')   
+if 'LR + transformer' in options:
+    model3 = joblib.load('LR_model.pkl')
+
+
+query = st.text_input("Enter your question")#user input
+button = st.button("Submit")
+
+
 #tfidf vectorizer and transformer
 vectorizer=joblib.load('tfidf_vectorizer.pkl')
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 
-# Initialize the Flask app
-app = Flask(__name__)
 
-# Define the API route
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Transform the user input into its embedding for lR
+text= spacy_tokenizer(query)
+text= model.encode(text).reshape(1,-1)
 
-@app.route('/predict/<selected_model>', methods=['POST'])
-def predict(selected_model):
-    try:
-        # Get the input text from the request
-        data = request.get_json(force=True)
-        text = data['text']
-        #selected_model = data['selected_model']
-        #calling the  functions for text pre processing
-        text= spacy_tokenizer(text)
 
-        # Transform the user input into its TF-IDF representation for svm knn
-        input_tfidf = vectorizer.transform([text])
 
-        # Transform the user input into its embedding for lR
-        text= spacy_tokenizer(text)
-        text= model.encode(text).reshape(1,-1)
+if button and query:
+    if 'SVM + tfidf' in options:
+        with st.spinner("Predicting..."):# if select this option
+            #pre-process text
+            text= spacy_tokenizer(query)
+            # Transform the user input into its TF-IDF representation for svm knn
+            input_tfidf = vectorizer.transform([text])
+            #predicting
+            answer = model1.predict(input_tfidf)[0]
+            #display
+            st.success("Predicted Disease: "+answer)
+    if 'KNN + tfidf' in options:
+        with st.spinner("Predicting..."):# if select this option
+            #pre-process text
+            text= spacy_tokenizer(query)
+            # Transform the user input into its TF-IDF representation for svm knn
+            input_tfidf = vectorizer.transform([text])
+            #predicting
+            answer = model2.predict(input_tfidf)[0]
+            #display
+            st.success("Predicted Disease: "+answer)
 
-        # Make the prediction using the ML model
-            
-        if selected_model == 'model1':
-            prediction = model1.predict(input_tfidf)[0]
-        elif selected_model == 'model2':
-            prediction = model2.predict(input_tfidf)[0]
-        elif selected_model == 'model3':
+    if 'LR + transformer' in options:# if select ask a question option
+        with st.spinner("Searching for the answer..."):
+            #pre-process text
+            text= spacy_tokenizer(query)
+            # Transform the user input into its embedding for lR
+            text= model.encode(text).reshape(1,-1)
+            #predicting
             dic= get_dict()
             i=model3.predict(text)[0]#(op is from 0-40 unique labels)
-            prediction = dic[i]#decode and get the disease
-        else:
-            return jsonify({'error': 'Invalid model selection.'})
+            answer = dic[i]#decode and get the disease
+            #display
+            st.success("Predicted Disease: "+answer)
 
-        # Return the prediction as a JSON response
-        response = {
-            'prediction': prediction
-        }
-        
-        return jsonify(response)
 
-    except Exception as e:
-        # Handle any errors and return an error message
-        return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    # Run the Flask app on localhost with port 5000
-    app.run(host='0.0.0.0', port=5000)
+
